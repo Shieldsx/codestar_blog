7b6e1b34e71cb73a14e195b933098a1fbf4ac856
Frank <fredhltomlinson@gmail.com>
2026-01-28 20:47:28 +0000

    Fixed database settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -80,9 +80,12 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
