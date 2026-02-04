4640e07d80d45fb7d62cd91d2afe3ead8f6fd12d
Frank <fredhltomlinson@gmail.com>
2026-02-04 11:08:06 +0000

    Added filter by status

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+    
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("body", "author", "post", "created_on", "approved")
@@ -13,4 +21,4 @@ class CommentAdmin(admin.ModelAdmin):
         queryset.update(approved=True)
 
 
-admin.site.register(Post)
+
