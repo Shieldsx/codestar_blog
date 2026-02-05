ceda795bba7123e31814fa4d00036d6ccff92691
Frank <fredhltomlinson@gmail.com>
2026-02-05 11:15:35 +0000

    added post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path("", views.PostList.as_view(), name="home"),      # alias
+    path("", views.PostList.as_view(), name="blog_home"), # optional keep both
+    path("<slug:slug>/", views.post_detail, name="post_detail"),
 ]
+
+
